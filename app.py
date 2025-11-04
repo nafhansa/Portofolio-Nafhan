6da50f5f3cf6047d7ecaaae94c5b73132a2557e7
nafhansa <nafhan1723@gmail.com>
2025-11-05 02:24:10 +0700

    remove local HF model to speed up railway build + fix app

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def init_vectorstore():
 
     # embedding lokal
     embeddings = HuggingFaceEmbeddings(
-        model_name="./models/all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf"
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
 
